3a8792f2a55f03e8ce4903f9d7cede2a42d403d5
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-15 22:21:27 +0300

    feat: style the <header> element and its children
    - Make the <header> element a flex container
    - Justify its content with space-between
    - Set the font sizes of the nested <h1> and <p> elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 export function Header() {
   return (
-    <header>
+    <header className="flex justify-between">
       <div>
-        <h1>Memory Card</h1>
-        <p>
+        <h1 className="text-4xl">Memory Card</h1>
+        <p className="text-lg">
           Get points by clicking an image, but be sure to not click more than
           once!
         </p>
       </div>
-      <div>
+      <div className="text-lg">
         <p>Score</p>
         <p>Best Score</p>
       </div>
