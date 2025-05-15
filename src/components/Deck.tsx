9c8e11a489f987e5dbe16bfa13a405f9b0567b99
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-15 20:36:32 +0300

    feat: use tanstack-query to fetch and render deck of cards

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { type ICard, Card } from "./Card";
 
 interface IDeck {
@@ -7,13 +8,32 @@ interface IDeck {
   remaining: number;
 }
 
-export function Deck(props: { deck: IDeck }) {
+export function Deck() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["deck"],
+    queryFn: async (): Promise<IDeck> => {
+      const response = await fetch(
+        "https://www.deckofcardsapi.com/api/deck/new/draw/?count=10"
+      );
+
+      return await response.json();
+    },
+  });
+
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>An error has occured: {error.message}</p>;
+  }
+
   return (
     <>
-      {props.deck.success &&
-        props.deck.cards.map((card) => {
-          <Card key={card.code} card={card} />;
-        })}
+      {data.success &&
+        data.cards.map((card) => <Card key={card.code} card={card} />)}
+
+      <div>{isFetching ? "Updating..." : ""}</div>
     </>
   );
 }
