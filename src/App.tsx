af2217f5c7fdbd1f4b41b8a50b9e2d371fbcd3d5
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-15 22:53:11 +0300

    feat: wrap the App component's body in a ScoreContext.Provider
    - Create the score, and setScore React state variables
    - Create the bestScore local variable. It can be calculated from score
    - Create the updateScore function
    - Pass the score, bestScore, and updateScore values to the Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Deck } from "./components/Deck";
 import { Header } from "./components/Header";
+import { useState } from "react";
+import { ScoreContext } from "./ScoreContext";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  let bestScore = 0;
+
+  const updateScore = () => {
+    let currentScore = score;
+    setScore((currentScore += 1));
+  };
+
+  if (score > bestScore) {
+    bestScore = score;
+  }
+
   return (
-    <>
+    <ScoreContext.Provider value={{ score, bestScore, updateScore }}>
       <Header />
       <QueryClientProvider client={queryClient}>
         <main className="grid grid-cols-5 gap-2">
           <Deck />
         </main>
       </QueryClientProvider>
-    </>
+    </ScoreContext.Provider>
   );
 }
 
