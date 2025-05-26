7cb6aa8439212834a2dac9c5b3b03d22811379cc
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-26 18:01:07 +0300

    feat: add a pressed effect to Cards when clicked

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ScoreContext } from "../ScoreContext";
 
 export interface ICard {
@@ -15,15 +15,23 @@ export interface ICard {
 export function Card(props: { card: ICard; key: string }) {
   const value = props.card.value + " of " + props.card.suit;
   const { updateScore } = useContext(ScoreContext);
+  const [pressed, setPressed] = useState(false);
 
   return (
-    <figure className="px-2 pt-2 pb-4 cursor-pointer">
+    <figure
+      className={`px-2 pt-2 pb-4 cursor-pointer transition-all duration-75 ${
+        pressed ? "translate-y-0.5 scale-95 shadow-inner" : ""
+      }`}
+    >
       <img
         src={props.card.image}
         alt={value}
         onClick={() => {
           updateScore();
         }}
+        onMouseDown={() => setPressed(true)}
+        onMouseUp={() => setPressed(false)}
+        onMouseLeave={() => setPressed(false)}
       />
       <figcaption className="text-center pt-1">{value}</figcaption>
     </figure>
